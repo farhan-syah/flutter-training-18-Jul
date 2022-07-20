3f54e9b88381ae7734626ae3984d864f712d8710
Farhan Syah <bizimpulse@gmail.com>
2022-07-20 10:07:09 +0800

    Add Appbar to Home Screen

--- lib/home.screen.dart
+++ lib/home.screen.dart
@@ -1,47 +1,35 @@
 import 'package:flutter/material.dart';
 
-import 'box.widget.dart';
-
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int numberOfBox = 0;
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo.shade800,
       appBar: AppBar(
-        title: const Text(
-          'Flutter Training',
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Wrap(
-            spacing: 10,
-            runSpacing: 10,
-            children: List.generate(
-              numberOfBox,
-              (index) => Box(
-                color: Colors.teal,
-                number: index,
-              ),
+        leading: const Icon(Icons.menu),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.white),
+                  child: const CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/images/profile_pic_men.webp')),
+                ),
+              ],
             ),
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          numberOfBox++;
-          setState(() {});
-        },
+        ],
       ),
     );
   }
